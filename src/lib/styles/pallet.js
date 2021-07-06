7cb03bdf4a42c3d9fc9ae9522181f975b3af46fd
M.W <auddnjs2008@naver.com>
2021-07-06 20:10:40 +0900

    로그인 , 회원가입, 첫 home 페이지 대략적인 ui완성,리덕스 스토어 연결

---
+++ src/lib/styles/pallet.js
@@ -0,0 +1,14 @@
+const pallet = {
+  green: ["#cff09e", "#a8dba8", "#79bd9a", "#3b8686"],
+  black: [
+    "#f5f7f3fd",
+    "#f2f3f1",
+    "#d5d8d0",
+    "#91948c",
+    "#666962",
+    "#2a2c26",
+    "#1b1d18",
+  ],
+};
+
+export default pallet;
